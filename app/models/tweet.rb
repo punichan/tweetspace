e7fd74145a2819e2607a87100cddbcf9cb7a3912
市川真巳 <masamikai4580@gmail.com>
2019-12-04 17:07:58 +0900

    いいね機能実装・フォロー機能のmodelとコントローラーの作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :delete_all
+  has_many :likes
   mount_uploader :image, ImageUploader
+
+  # current_userがいいねしているかどうかを判定してくれる
+  def like_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: 'tweets#index'
-  resources :users
+
+  resources :users do
+    resources :relationships, only: [:create, :destroy]
+    get :follows , on: :member
+    get :followers , on: :member
+  end
+
   resources :tweets do
     resources :comments, only: [:create, :edit, :update, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
