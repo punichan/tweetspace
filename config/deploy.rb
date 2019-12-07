da07bb5cde05f154f9876613441c99759ec21826
市川真巳 <masamikai4580@gmail.com>
2019-12-07 21:26:29 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'tweetspace'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:punityan/tweetspace.git'
+set :repo_url,  'https://github.com/punichan/tweetspace.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
