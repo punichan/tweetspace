efd3f0858a426ffa647a0ef2bc13805b60aa91c5
市川真巳 <masamikai4580@gmail.com>
2020-01-03 14:51:55 +0900

    gem faker seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times do |n| 
+
+Tweet.create(
+  tweet: Faker::Name.name,
+  food: Faker::Name.name,
+  price: "100",
+  store: Faker::Name.name,
+  image: File.open("./app/assets/images/default.jpg"),
+  place_id: "1",
+  category_id: "1",
+  user_id: "1",
+)
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
-    password = Faker::Internet.password(8)
     avater                 {Faker::Avatar.image}
     sequence(:name)        {Faker::Name.last_name}
-    password               { password }
-    password_confirmation  { password }
+    password               {"password"}
+    password_confirmation  {"password" }
     sequence(:email)       {Faker::Internet.email}
     created_at             {Faker::Time.between(from: DateTime.now - 9, to: DateTime.now) }
   end
