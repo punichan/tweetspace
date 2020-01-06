bd035a8e6785db0b9a124f5f3b11514f1a936e82
市川真巳 <masamikai4580@gmail.com>
2020-01-06 21:28:13 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 
 
-50.times do |n| 
+# 50.times do |n| 
 
-Tweet.create(
-  tweet: Faker::Name.name,
-  food: Faker::Name.name,
-  price: "100",
-  store: Faker::Name.name,
-  image: File.open("./app/assets/images/default.jpg"),
-  place_id: "1",
-  category_id: "1",
-  user_id: "1",
-)
-end
+# Tweet.create(
+#   tweet: Faker::Name.name,
+#   food: Faker::Name.name,
+#   price: "100",
+#   store: Faker::Name.name,
+#   image: File.open("./app/assets/images/default.jpg"),
+#   place_id: "1",
+#   category_id: "1",
+#   user_id: "1",
+# )
+# end
