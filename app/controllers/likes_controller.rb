c227300a7a7775f1fdc192defd7215180dd86252
市川真巳 <masamikai4580@gmail.com>
2020-01-19 17:04:10 +0900

    非同期でいいね機能を実装
    remote:trueにて非同期通信によるいいね機能が実装できた。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
   before_action :move_to_signup
   def create
+    @tweet = Tweet.find(params[:tweet_id])
     like = current_user.likes.new(tweet_id: params[:tweet_id])
     like.save!
-    redirect_to tweet_path(params[:tweet_id])
   end
 
   def destroy
+    @tweet = Tweet.find(params[:tweet_id])
     like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     like.destroy
-    redirect_to user_path(current_user.id)
   end
 
   private
