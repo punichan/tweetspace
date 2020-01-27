e50411885e1aff60c96248497187d213790e36ed
市川真巳 <masamikai4580@gmail.com>
2020-01-27 19:01:51 +0900

    コントローラのリファクタリング
    tweetコントローラの同じ記述をbefore_actionでまとめた。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
   before_action :move_to_signup, only: [:new, :create, :edit, :update, :destroy]
- 
+  before_action :tweet_find, only: [:show, :edit, :update, :destroy, ]
+  before_action :user_find, only: [:follows, :likes, :mytweets, :comment_tweets]
   def top
     @tweets = Tweet.order("created_at DESC").limit(8)
     @users = User.order("created_at DESC").limit(8)
@@ -34,23 +35,19 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
     @comments = @tweet.comments
     @comment = Comment.new
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
-    @tweet = Tweet.find(params[:id])
     @tweet.update(tweet_params)
     redirect_to tweet_path(@tweet.id)
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
     if @tweet.user_id == current_user.id
       @tweet.destroy
       redirect_to user_path(current_user.id)
@@ -60,26 +57,22 @@ class TweetsController < ApplicationController
   end
 
   def follows
-    @user = User.find(params[:format])
     @finduser = User.find(current_user.id)
     @follows = @finduser.followings
     @FolTws = Tweet.where(user_id: @follows.ids).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def likes
-    @user = User.find(params[:format])
     @likes = @user.likes
     @LikTws = @likes.map{|like| Tweet.find(like.tweet_id)}
     @LikTws = Kaminari.paginate_array(@LikTws).page(params[:page]).per(50)
   end
 
   def mytweets
-    @user = User.find(params[:format])
     @mytweets = Tweet.where(user_id: params[:format]).order("created_at DESC").page(params[:page]).per(50)
   end
 
   def comment_tweets
-    @user = User.find(params[:format])
     @comments = @user.comments
     @comment_tweets = @comments.map{|comment| Tweet.find(comment.tweet_id)}
     @comment_tweets = Kaminari.paginate_array(@comment_tweets).page(params[:page]).per(50)
@@ -89,6 +82,14 @@ class TweetsController < ApplicationController
   
   def tweet_params
     params.require(:tweet).permit(:tweet, :user_id, :place_id, :category_id, :food, :price, :store, :image)
+  end
+  
+  def tweet_find
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def user_find
+    @user = User.find(params[:format])
   end
   
 end
