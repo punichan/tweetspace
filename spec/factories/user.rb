809b2459497151748ff18ec34fd5c9c273aaf711
市川真巳 <masamikai4580@gmail.com>
2020-01-30 10:23:23 +0900

    factorybot userの作成

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     avater                 {Faker::Avatar.image}
     sequence(:name)        {Faker::Name.last_name}
+    profile                {Faker::Name.last_name}
     password               {"password"}
     password_confirmation  {"password" }
     sequence(:email)       {Faker::Internet.email}
