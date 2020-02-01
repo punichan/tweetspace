84a6f712d447707170c83e40fa8f55be5f74c116
市川真巳 <masamikai4580@gmail.com>
2020-02-02 00:37:49 +0900

    fackerの編集

--- spec/factories/tweet.rb
+++ spec/factories/tweet.rb
@@ -2,8 +2,14 @@ FactoryBot.define do
   factory :tweet do
     tweet      {Faker::Name.last_name}
     image      {Faker::Avatar.image}
+    food       {Faker::Dessert.flavor}
+    price      {1000}
+    store      {Faker::Dessert.flavor}
     created_at {Faker::Time.between(from: DateTime.now - 9, to: DateTime.now) }
     updated_at {Faker::Time.between(from: DateTime.now - 9, to: DateTime.now) }
+    place_id   {1}
+    category_id{1}
     user
+
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,7 +7,13 @@ describe Comment do
       expect(comment.errors[:comment]).to include("を入力してください")
     end
 
-    it "tweet_idがなかったら登録できない" do
+    it "200文字以上では登録できない" do
+      comment = build(:comment, comment:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      comeent.vaild?
+      expect(comment.errors[:comment]).to include("200文字以内で入力してください")
+    end
+    
+      it "tweet_idがなかったら登録できない" do
       comment = build(:comment, tweet_id:"")
       comment.valid?
       expect(comment.errors[:tweet_id]).to include("を入力してください")
