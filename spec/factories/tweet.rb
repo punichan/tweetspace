6d7d2ca76b94e684f97086d4790329c26613c1a0
市川真巳 <masamikai4580@gmail.com>
2020-02-04 15:32:49 +0900

    テスト
    tweetの単体テスト

--- spec/factories/tweet.rb
+++ spec/factories/tweet.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :tweet do
     tweet      {Faker::Name.last_name}
     image      { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
- 
     food       {Faker::Dessert.flavor}
     price      {1000}
     store      {Faker::Dessert.flavor}
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -4,24 +4,57 @@ RSpec.describe Tweet, type: :model do
     context 'can not save' do
       it "tweetがなかったら登録できない" do
         tweet = build(:tweet, tweet:"")
-        
         tweet.valid?
         expect(tweet.errors[:tweet]).to include("を入力してください")
       end
+
       it "user_idがなかったら登録できない" do
         tweet = build(:tweet, user_id:"")
         tweet.valid?
         expect(tweet.errors[:user_id]).to include("を入力してください")
       end
+
+      it "place_idがなかったら登録できない" do
+        tweet = build(:tweet, place_id:"")
+        tweet.valid?
+        expect(tweet.errors[:place_id]).to include("を入力してください")
+      end
+
+      it "category_idがなかったら登録できない" do
+        tweet = build(:tweet, category_id:"")
+        tweet.valid?
+        expect(tweet.errors[:category_id]).to include("を入力してください")
+      end
+
+      it "priceがなかったら登録できない" do
+        tweet = build(:tweet, price:"")
+        tweet.valid?
+        expect(tweet.errors[:price]).to include("を入力してください")
+      end
+
+      it "foodがなかったら登録できない" do
+        tweet = build(:tweet, food:"")
+        tweet.valid?
+        expect(tweet.errors[:food]).to include("を入力してください")
+      end
+
+      it "imageがなかったら登録できない" do
+        tweet = build(:tweet, image:"")
+        tweet.valid?
+        expect(tweet.errors[:image]).to include("を入力してください")
+      end
+
+      it "storeがなかったら登録できない" do
+        tweet = build(:tweet, store:"")
+        tweet.valid?
+        expect(tweet.errors[:store]).to include("を入力してください")
+      end
     end
 
     context 'can save' do
-      it "tweetがあったら登録できる" do
-        tweet = create(:tweet, image:"")
+      it "全てあれば登録できる" do
+        tweet = build(:tweet)
         expect(tweet).to be_valid
-      end
-      it "tweetとimageがあったら登録できる" do
-        expect(create(:tweet)).to be_valid
       end
     end
   end
